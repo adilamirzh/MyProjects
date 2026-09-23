dc404cd4d092c73f12dd199f0c2c39b2290e1190
adilamirzh <amirzhanov.adil1511@gmail.com>
2026-09-23 12:04:38 +0800

    Add files via upload

---
+++ Linear Regression.py
@@ -0,0 +1,152 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+# data is stored in numpy array/matrix
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
+
+# def predict_single_loop(x, w, b):
+#
+#     n = x.shape[0]
+#     p = 0
+#     for i in range(n):
+#         p_i = x[i] * w[i]
+#         p = p + p_i
+#     p = p + b
+#     return p
+#
+#
+# x_vec = X_train[0,:]
+# #print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+#
+#
+# f_wb = predict_single_loop(x_vec, w_init, b_init)
+# print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+
+
+def predict(x, w, b):
+    p = np.dot(x, w) + b
+    return p
+
+
+x_vec = X_train[0,:]
+print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+f_wb = predict(x_vec,w_init, b_init)
+print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+
+
+def compute_cost(X, y, w, b):
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b
+        cost = cost + (f_wb_i - y[i]) ** 2
+    cost = cost / (2 * m)
+    return cost
+
+# Compute and display cost using our pre-chosen optimal parameters.
+cost = compute_cost(X_train, y_train, w_init, b_init)
+print(f'Cost at optimal w : {cost}')
+
+
+def compute_gradient(X, y, w, b):
+
+    m, n = X.shape  # (number of examples, number of features)
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):
+        err = (np.dot(X[i], w) + b) - y[i]
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err * X[i, j]
+        dj_db = dj_db + err
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_db, dj_dw
+
+
+tmp_dj_db, tmp_dj_dw = compute_gradient(X_train, y_train, w_init, b_init)
+print(f'dj_db at initial w,b: {tmp_dj_db}')
+print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
+
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
+
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  # avoid modifying global w within function???
+    b = b_in
+
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = gradient_function(X, y, w, b)  ##None
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw  ##None
+        b = b - alpha * dj_db  ##None
+
+        # Save cost J at each iteration
+        if i < 100000:  # prevent resource exhaustion
+            J_history.append(cost_function(X, y, w, b))
+
+
+    return w, b, J_history  # return final w,b and J history for graphing
+
+
+initial_w = np.zeros_like(w_init)
+initial_b = 0.
+
+iterations = 10
+alpha = 5.0e-7
+
+w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b,
+                                                    compute_cost, compute_gradient,
+                                                    alpha, iterations)
+print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+m,_ = X_train.shape
+# for i in range(m):
+#     print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
+#
+#     # plot cost versus iteration
+fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
+ax1.plot(J_hist)
+ax2.plot(5+np.arange(len(J_hist[5:])), J_hist[5:])
+ax1.set_title("Cost vs. iteration");
+ax2.set_title("Cost vs. iteration")
+ax1.set_ylabel('Cost');
+ax2.set_ylabel('Cost')
+ax1.set_xlabel('iteration step');
+ax2.set_xlabel('iteration step')
+
+
+fig2=plt.figure(figsize=(10,7))
+ax3=fig2.add_subplot(projection="3d")
+
+ax3.plot(X_train[:, 0],X_train[:, 1], y_train, color="blue", label="DATA")
+y_pred=np.dot(X_train, w_final)+b_final
+ax3.plot(X_train[:, 0],X_train[:, 1], y_pred, color="red", label="ML")
+ax3.set_xlabel("x1")
+ax3.set_ylabel("x2")
+ax3.set_zlabel("y", color="blue")
+ax3.legend()
+
+
+plt.title("data/model")
+plt.show()
